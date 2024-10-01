a79dfc28837367bc9a7934333c2105e633ae3494
WarrenAdams8 <warren.codes@outlook.com>
2024-10-01 12:58:15 -0400

    Added intial Search functionality

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Index page</h1>
+    <p>Search Query is: {{ debouncedSearchQuery }}</p>
+    <input v-model="searchQuery" placeholder="search" />
+    <p v-if="status == 'pending'">Loading...</p>
+    <template v-else>
+        <li v-for="book in books.items">
+            {{ book.volumeInfo.title }}
+            <img :src=book.volumeInfo.imageLinks.smallThumbnail />
+        </li>
+    </template>
+</template>
+<script setup lang="ts">
+const searchQuery = ref('Harry Potter')
+const debouncedSearchQuery = refDebounced(searchQuery, 500)
+
+const { data: books, status } = await useLazyAsyncData(
+    'books',
+    () => $fetch(`https://www.googleapis.com/books/v1/volumes?q=${debouncedSearchQuery.value ? debouncedSearchQuery.value : 'Harry potter'}&maxResults=30&filter=paid-ebooks`), {
+    watch: [debouncedSearchQuery]
+}
+)
+
+</script>
